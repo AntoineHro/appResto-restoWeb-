90ecf4fb5710513d5ae9019832711c24ad423bf1
Antoine Haro <antoine.haro@limayrac.fr>
2025-09-26 11:13:40 +0200

    Delete appRoseBlanche directory

--- appRoseBlanche/page/script/recuperationUtil_script.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_SESSION['idUtilisateur'])) {
-    $stmt = $pdo->prepare("SELECT * FROM Utilisateurs WHERE idUtilisateur = :idUtilisateur");
-    $stmt->execute([':idUtilisateur' => $_SESSION['idUtilisateur']]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-}
-?>
