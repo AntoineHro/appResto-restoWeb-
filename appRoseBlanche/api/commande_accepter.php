2300b7b880cb5d5dc4f612464755f1168311cecd
Antoine Haro <antoine.haro@limayrac.fr>
2026-04-02 17:41:20 +0200

    Add files via upload
    Ajout dernière version

---
+++ appRoseBlanche/api/commande_accepter.php
@@ -0,0 +1,13 @@
+<?php
+    require_once '../page/script/connexion_dbh.php';
+
+    $id_commande = $_GET['id_commande'];
+
+    
+    $stmt = $pdo->prepare("UPDATE Commandes SET idEtat=6 where idCommande=:id_commande");
+    $stmt->execute([':id_commande' => $id_commande]);
+
+    // requête API http://localhost/projet/appRoseBlanche/api/commande_accepter.php?id_commande= <-- on ajoute l'id de la commande
+
+    // processus de notification 
+?>
